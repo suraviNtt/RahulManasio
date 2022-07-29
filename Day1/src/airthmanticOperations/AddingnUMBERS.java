cb80a6b935926ea8ada2e040ef5ea3c33f6f3382
suraviNtt <93370272+suraviNtt@users.noreply.github.com>
2022-07-29 15:54:17 +0530

    opps concept

---
+++ Day1/src/airthmanticOperations/AddingnUMBERS.java
@@ -0,0 +1,13 @@
+package airthmanticOperations;
+
+public class AddingnUMBERS {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		int c = x+y;
+		System.out.println("sum = "+c);
+		System.out.println("Sum of "+x+" and "+y+" is equal to : "+c);
+	}
+}
